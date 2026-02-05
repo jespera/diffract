467afab333c580a98c80fbee80545b844a3c769a
Jesper Andersen <jesper.andersen@deondigital.com>
2026-02-05 12:04:24 +0100

    Initial Scala support

--- tests/test_runner.ml
+++ tests/test_runner.ml
@@ -6,6 +6,7 @@ let () =
     ("Match", Test_match.tests);
     ("Match (Kotlin)", Test_match.kotlin_tests);
     ("Match (PHP)", Test_match.php_tests);
+    ("Match (Scala)", Test_match.scala_tests);
     ("Match (Field)", Test_match.field_tests);
     ("Match (Ellipsis)", Test_match.ellipsis_tests);
   ]
